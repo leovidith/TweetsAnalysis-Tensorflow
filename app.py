860c4182f69388e50fdc50afac616d6d41896d1a
Vidith <kotasaividith@gmail.com>
2024-11-11 08:01:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,24 @@ import tensorflow_hub as hub
 import numpy as np
 import re
 
+
 app = Flask(__name__)
 
-model = tf.keras.models.load_model(r"path_to_your_model", custom_objects={'KerasLayer': hub.KerasLayer})
+try:
+    model = tf.keras.models.load_model(r"Model.h5", custom_objects={'KerasLayer': hub.KerasLayer})
+    print("Model load ✅")
+except:
+    print("Model loading ❌")
 
 sentiment_labels = {0: 'Negative 🤬', 1: 'Neutral 😐', 2: 'Positive 😊'}
 
 def clean(text):
-    """Clean input text by removing non-alphabet characters and converting to lowercase."""
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     text = text.lower()
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
 def predict_sentiment(sentence):
-    """Predict the sentiment of a given sentence."""
     sentence = clean(sentence)
     sentence = np.array([sentence])
     preds = model.predict(sentence)
@@ -28,7 +31,6 @@ def predict_sentiment(sentence):
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    """Render home page with a form and handle form submissions for sentiment prediction."""
     if request.method == 'POST':
         user_input = request.form.get('sentence')
         if user_input:
@@ -36,5 +38,5 @@ def home():
             return render_template('index.html', prediction=sentiment, user_input=user_input)
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+app.run(debug=False)
